712a9df2d0660944ee91442a1835ec8bf1017b1b
glbls <3350@duck.com>
2025-03-23 11:25:53 +0200

    Add user input with fmt.Scan

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
+
 import "fmt"
-func main(){
-	fmt.Println("Hello world!")
+
+func main() {
+    var name string
+    var age int
+    var hobby string
+
+    fmt.Print("Как вас зовут? ")
+    fmt.Scan(&name)
+
+    fmt.Print("Сколько вам лет? ")
+    fmt.Scan(&age)
+
+    fmt.Print("Какое у вас хобби? ")
+    fmt.Scan(&hobby)
+
+    if age < 0 {
+        fmt.Println("Возраст не может быть отрицательным!")
+    } else {
+        fmt.Println("Привет,", name, "! Тебе", age, "лет, и твоё хобби -", hobby)
+    }
 }
