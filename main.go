98ce418496ac09b7225f3d2c453c277d7ccdf249
glbls <3350@duck.com>
2025-03-26 07:12:22 +0200

    Day 8: full name and 5 string tasks

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-func main() {
+func getName() {
 	var name, lastName, fullName string
 	fmt.Println("Введите имя:")
 	fmt.Scan(&name)
@@ -23,4 +23,53 @@ func main() {
 	fmt.Println("Полное имя:", fullName)
 	fmt.Println("Длина:", len(fullName))
 	fmt.Println("Верхний регистр:", strings.ToUpper(fullName))
+}
+func lenString() {
+	var getStr string
+	fmt.Println("Введите строку:")
+	fmt.Scan(&getStr)
+	fmt.Println("Длина строки:", len(getStr))
+}
+func twoStr() {
+	var str1, str2 string
+	fmt.Println("Введите первую строку:")
+	fmt.Scan(&str1)
+	fmt.Println("Введите вторую строку:")
+	fmt.Scan(&str2)
+	fmt.Println("Объединенные строки с дефисом", str1+"-"+str2)
+}
+func inclA() {
+	var str string
+	fmt.Println("Введите строку:")
+	fmt.Scan(&str)
+	if strings.Contains(str, "a") || strings.Contains(str, "A") {
+		fmt.Println("Строка содержит букву a.")
+	} else {
+		fmt.Println("Строка не содержит букву а.")
+	}
+}
+func lowReg() {
+	var str string
+	fmt.Println("Введите строку:")
+	fmt.Scan(&str)
+	fmt.Println("Строка в нижнем регистре:", strings.ToLower(str))
+}
+func lengthStr() {
+	var str string
+	fmt.Println("Введите строку:")
+	fmt.Scan(&str)
+	if len(str) > 5 {
+		fmt.Println("Да")
+	} else {
+		fmt.Println("Нет")
+	}
+}
+func main() {
+
+	lengthStr()
+	lowReg()
+	inclA()
+	twoStr()
+	getName()
+	lenString()
 }
